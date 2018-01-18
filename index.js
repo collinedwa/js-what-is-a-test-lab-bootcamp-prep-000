18b42d78e016df5f136fa6b2c626d225474d0664
Collin Edwards <collinedwards1579@gmail.com>
2018-01-18 22:28:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
-var name = "Susan"
-var height = 3.00005
-var message = ` is ${height} inches tall`
+var name = "Susan";
+var height = 3.00005;
+var message = ` is ${height} inches tall`;
 
 module.exports = {name, height, message}
